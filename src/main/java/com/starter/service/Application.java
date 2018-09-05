228e75d050718da22641d9a31f079025154199c3
Vener Guevarra <vnrguevarra@gmail.com>
2018-09-05 11:16:32 +0800

    Added entity package scan.

--- src/main/java/com/starter/service/Application.java
+++ src/main/java/com/starter/service/Application.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
@@ -16,9 +17,11 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
+@EntityScan(basePackages = { Application.ENTITY_PACKAGE })
 public class Application {
     private static final int MESSAGE_CACHE_SECONDS = 3600;
-
+    protected static final String ENTITY_PACKAGE = "com.starter.service.model";
+    
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws IOException {
